0e25c4c1f75a05843223f110fdf1ed8550b0f791
Alan Lee <alanleeone@gmail.com>
2020-06-25 18:07:53 -0700

    Updates

---
+++ precache-manifest.79474d94474ce2527081414a7e95489d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc89fa214258cd46028a239fa08afb56",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "4029b51bd0c2f38f34fc",
+    "url": "/reactportfolio/static/css/main.cdc5a744.chunk.css"
+  },
+  {
+    "revision": "ac214172ae0930f6444b",
+    "url": "/reactportfolio/static/js/2.66b78a01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.66b78a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4029b51bd0c2f38f34fc",
+    "url": "/reactportfolio/static/js/main.7371ddc4.chunk.js"
+  },
+  {
+    "revision": "5bc3b9fba7145f4476a9",
+    "url": "/reactportfolio/static/js/runtime-main.bf6de848.js"
+  }
+]);
